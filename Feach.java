014e56ee2fb1767ec8e920758e38f50edde8824c
VTVRam <vtvramchowdary@gmail.com>
2025-10-08 10:26:44 +0530

    Create Feach.java

---
+++ Feach.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Feach
+{
+	public static void main(String[] args)
+	{
+		System.out.println("list is");
+		int[] list={90,80,70,60};
+		for(int i:list)
+		{
+			System.out.println(i);
+		}
+	}
+}
